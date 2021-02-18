2e1f298cf10d18e2206a7487924702654405140f
kensuke suzuki <35k.0101@gmail.com>
2021-02-18 14:10:50 +0900

    レスポンシブに対応

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function changeImage(anything) {
   document.getElementById("slider").src = anything;
 }
+
+function toggleMenu() {
+  const munuToggle = document.querySelector(".menuToggle");
+  const navigation = document.querySelector(".navigation");
+  munuToggle.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
